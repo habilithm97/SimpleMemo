d1627b2a2c8311fb2079c1a43606c5f770e40028
habilithm97 <habinkim97@gmail.com>
2024-10-14 23:45:50 +0900

    메인 화면 (View Binding)

--- app/src/main/java/com/example/simplememo/ui/MainActivity.kt
+++ app/src/main/java/com/example/simplememo/ui/MainActivity.kt
@@ -1,20 +1,30 @@
-package com.example.simplememo
+package com.example.simplememo.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.simplememo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        init()
+    }
+
+    private fun init() {
+        binding.apply {
+
         }
     }
 }

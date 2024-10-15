dfd4919323985ef541905d96ba0b875aa7dd6048
habilithm97 <habinkim97@gmail.com>
2024-10-15 23:48:18 +0900

    RecyclerView Adapter (ListAdapter)

---
+++ app/src/main/java/com/example/simplememo/adapter/MemoAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.simplememo.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class MemoAdapter : ListAdapter<Memo, MemoAdapter.MemoViewHolder>(DIFF_CALLBACK) {
+
+    inner class MemoViewHolder(private val binding: ItemMemoBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+            fun bind(memo: Memo) {
+
+            }
+    }
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Memo>() {
+            override fun areItemsTheSame(oldItem: Memo, newItem: Memo): Boolean {
+                return oldItem.id == newItem.id
+            }
+            override fun areContentsTheSame(oldItem: Memo, newItem: Memo): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemoViewHolder {
+        val binding = ItemMemoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MemoViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MemoViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
